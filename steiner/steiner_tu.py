c667c0ae415a115ee73e08975ab096aa47a0af44
Andre Schidler <andre@schidler.at>
2018-02-15 23:37:45 +0100

    Removed signal handling to get a clear timeout exception

--- steiner/steiner_tu.py
+++ steiner/steiner_tu.py
@@ -7,26 +7,13 @@ import signal
 
 """ The real solver script that reads from stdin and outputs the solution """
 
-
-stop = False
 steiner = pp.parse_pace_file(sys.stdin)
 solver = cfg.solver(steiner)
-
-
-def stop_execution(a, b):
-    global stop, solver
-    stop = True
-    solver.stop = True
-
-
-signal.signal(signal.SIGINT, stop_execution)
-signal.signal(signal.SIGTERM, stop_execution)
 
 # Reduce, in case of a timeout let the reduction finish in the background. One cannot kill a thread
 reducers = cfg.reducers()
 for r in reducers:
-    if not stop:
-        r.reduce(steiner)
+    r.reduce(steiner)
 
 # Solve
 solver.solve()
@@ -34,8 +21,6 @@ solver.solve()
 solution = solver.result
 reducers.reverse()
 
-if stop:
-    exit(1)
 
 for r in reducers:
     solution = r.post_process(solution)
