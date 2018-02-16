a19cbba2335562f0c86dbd6d6b4de60ffc77bdad
WRK.AT\a.schidler <a.schidler@wrk.at>
2018-02-16 16:27:25 +0100

    Start of new terminal reduction

--- steiner/reduction/terminals.py
+++ steiner/reduction/terminals.py
@@ -9,6 +9,7 @@ class TerminalReduction:
     def __init__(self):
         self._removed = []
         self._selected = []
+        self._done = False
 
     def reduce(self, steiner):
         track = len(steiner.terminals)
@@ -18,28 +19,33 @@ class TerminalReduction:
             for t in list(steiner.terminals):
                 min_val = sys.maxint
                 min_node = None
-                neighbors = 0
+                neighbors = list(nx.neighbors(steiner.graph, t))
 
-                for n in nx.neighbors(steiner.graph, t):
-                    neighbors = neighbors + 1
+                for n in neighbors:
                     w = steiner.graph[t][n]['weight']
                     if w < min_val:
                         min_node = n
                         min_val = w
 
-                if min_node in steiner.terminals or neighbors == 1:
-                    if neighbors == 1:
+                if min_node in steiner.terminals or len(neighbors) == 1:
+                    # Only one neighbor? The edge has to be selected, so we can treat the neighbor as a terminal
+                    if len(neighbors) == 1:
                         self._removed.append((t, min_node, w))
                         steiner.graph.remove_node(t)
                         steiner.terminals.add(min_node)
                         steiner.terminals.remove(t)
                         change = True
+                    # The closest node is a terminal? The edge will be in an optimal solution, therefore merge the nodes
                     elif min_node in steiner.terminals:
-                        # TODO: Mechanism for preselection
-                        # Since the terminal is already preselection, do not treat as terminal
+                        self._removed.append((t, min_node, min_val))
+                        for ng in neighbors:
+                            if ng != min_node:
+                                d = steiner.graph[t][ng]['weight']
+                                if steiner.add_edge(min_val, ng, d + min_val):
+                                    self._selected.append(((min_val, ng, d + min_val), (t, ng, d)))
+
                         steiner.terminals.remove(t)
-                        steiner.graph[t][min_node]['selected'] = True
-                        self._selected.append((t, min_node, min_val))
+                        steiner.graph.remove_node(t)
                         change = True
 
         return track - len(steiner.terminals)
@@ -47,25 +53,20 @@ class TerminalReduction:
     def post_process(self, solution):
         cost = solution[1]
         change = False
-        for (n1, n2, w) in self._removed:
-            if not solution[0].has_edge(n1, n2):
-                solution[0].add_edge(n1, n2, weight=w)
-                cost = cost + w
-                change = True
 
-        # TODO: Idea for preselected edges: Whenever the solver expands a preselected edge, add the neighbor with costs 0
-        # TODO: Review if this is indeed correct
-        counter = 0
-        while counter != len(self._selected):
-            for (n1, n2, w) in self._selected:
-                # Already in the solution
-                if n1 in solution[0].nodes:
-                    counter = counter + 1
-                # The second conditional is to avoid introducing disconnected components
-                elif n1 not in solution[0].nodes and n2 in solution[0].nodes:
+        if not self._done:
+            for (n1, n2, w) in self._removed:
+                if not solution[0].has_edge(n1, n2):
                     solution[0].add_edge(n1, n2, weight=w)
                     cost = cost + w
                     change = True
-                    counter = counter + 1
+            self._done = True
+
+        for (e1, e2) in self._selected:
+            if solution[0].has_edge(e1[0], e1[1]):
+                if solution[0][e1[0]][e2[0]] == e1[2]:
+                    solution[0].remove_edge(e1[0], e1[1])
+                    solution[0].add_edge(e2[0], e2[1], weight=e2[2])
+                    change = True
 
         return (solution[0], cost), change
