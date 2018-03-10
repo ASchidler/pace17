af39bf3777ed96fc4edb3c1371b9ba957a86e3bb
Andre Schidler <andre.schidler@gmail.com>
2018-03-10 17:15:15 +0100

    Added debugging version of the set storage

--- steiner/set_storage.py
+++ steiner/set_storage.py
@@ -7,20 +7,6 @@ class SetStorage:
         self.initialized = True
         self.root.add(set_id)
 
-    def _health_check(self, n, bit_set):
-        if n is None:
-            return
-
-        if n.level > 0:
-            result = n.val & (1 << (n.level - 1))
-            if result > 0 and not bit_set:
-                print "!!! Set where 0"
-            elif result == 0 and bit_set:
-                print "!!! Unset where 1"
-
-        self._health_check(n.zer, False)
-        self._health_check(n.on, True)
-
     def findAll(self, set_id):
         if not self.initialized:
             return []
@@ -50,32 +36,28 @@ class SetStorage:
         return ret
 
     def findAllGen(self, set_id):
-        if not self.initialized:
-            return
+        if self.initialized:
 
-        ret = []
-        queue = []
+            queue = []
 
-        current_node = (self.root, (set_id & 1) > 0)
+            current_node = (self.root, (set_id & 1) > 0)
 
-        # Special case for root node, since no information from the previous node is available
-        while current_node[0] is not None or len(queue) > 0:
-            if current_node[0] is None:
-                current_node = queue.pop()
-            if current_node[0] is None:
-                continue
+            # Special case for root node, since no information from the previous node is available
+            while current_node[0] is not None or len(queue) > 0:
+                if current_node[0] is None:
+                    current_node = queue.pop()
+                if current_node[0] is None:
+                    continue
 
-            next_level_set = (set_id & (1 << (current_node[0].level + 1)))
+                next_level_set = (set_id & (1 << (current_node[0].level + 1)))
 
-            if (not current_node[1] and current_node[0].fixed) or (current_node[0].val & set_id) == 0:
-                ret.append(current_node[0].val)
+                if (not current_node[1] and current_node[0].fixed) or (current_node[0].val & set_id) == 0:
+                    yield current_node[0].val
 
-            if not current_node[1]:
-                queue.append((current_node[0].on, next_level_set))
+                if not current_node[1]:
+                    queue.append((current_node[0].on, next_level_set))
 
-            current_node = (current_node[0].zer, next_level_set)
-
-        return ret
+                current_node = (current_node[0].zer, next_level_set)
 
 
 class _SetNode:
@@ -173,6 +155,48 @@ class _SetStorageIterator:
         return []
 
 
+class DebuggingSetStorage(SetStorage):
+    """This adds self checks to the set storage. It is very slow and its use is to test changes, for
+    production use, use the base storage"""
+
+    def __init__(self):
+        self._check = []
+        SetStorage.__init__(self)
+
+    def _health_check(self, n, bit_set):
+        if n is None:
+            return
+
+        if n.level > 0:
+            result = n.val & (1 << (n.level - 1))
+            if result > 0 and not bit_set:
+                print "!!! Set where 0"
+            elif result == 0 and bit_set:
+                print "!!! Unset where 1"
+
+        self._health_check(n.zer, False)
+        self._health_check(n.on, True)
+
+    def add(self, set_id):
+        self._check.append(set_id)
+        SetStorage.add(self, set_id)
+
+    def findAllGen(self, set_id):
+        if self.initialized:
+            ref = self.findAll(set_id)
+            ref2 = set([x for x in self._check if (set_id & x) == 0])
+            if len(ref) != len(ref2):
+                print "*** Length Error {} to {}".format(len(ref), len(ref2))
+                raise StandardError
+            for x in ref:
+                if x not in ref2:
+                    print "*** Content Error {}".format(x)
+                    raise StandardError
+
+            self._health_check(self.root, (1 & self.root.val) > 0)
+
+            for x in SetStorage.findAllGen(self, set_id):
+                yield x
 
 
 
