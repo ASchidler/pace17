0eff8961e78540ef335bd3884ab418ba905393ff
Andre Schidler <andre@schidler.at>
2018-04-01 16:50:20 +0200

    Some Changes

--- steiner/solver/solver_bc.py
+++ steiner/solver/solver_bc.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import scipy.optimize as sp
+import sys
 
 
 class SolverBc:
@@ -14,7 +15,7 @@ class SolverBc:
         self.a_eq = []
         self.b_eq = []
 
-        self.nodes = list(nx.nodes(self.steiner.graph))
+        self.nodes = [n for n in nx.nodes(self.steiner.graph) if n not in self.terminals]
 
         self.terminals.sort()
         self.nodes.sort()
@@ -59,45 +60,42 @@ class SolverBc:
 
         # Non-Terminals have at max one incoming edge
         for n in self.nodes:
-            if n not in self.terminals and n != self.root:
-                arr = [0] * len(self.edges)
-                for n2 in nx.neighbors(steiner.graph, n):
-                    # Treat as incoming edge
-                    idx = self.edges.index((n2, n))
-                    arr[idx] = 1
+            arr = [0] * len(self.edges)
+            for n2 in nx.neighbors(steiner.graph, n):
+                # Treat as incoming edge
+                idx = self.edges.index((n2, n))
+                arr[idx] = 1
 
-                self.a_ub.append(arr)
-                self.b_ub.append(1)
+            self.a_ub.append(arr)
+            self.b_ub.append(1)
 
         # For Non-Terminals, more or equal have to exit than entered (according to paper, this strengthens relaxations)
         for n in self.nodes:
-            if n not in self.terminals and n != self.root:
-                arr = [0] * len(self.edges)
-                for n2 in nx.neighbors(steiner.graph, n):
-                    # Treat as incoming edge
-                    idx = self.edges.index((n2, n))
-                    arr[idx] = 1
-                    # Outgoing edge, should be larger
-                    idx = self.edges.index((n, n2))
-                    arr[idx] = -1
+            arr = [0] * len(self.edges)
+            for n2 in nx.neighbors(steiner.graph, n):
+                # Treat as incoming edge
+                idx = self.edges.index((n2, n))
+                arr[idx] = 1
+                # Outgoing edge, should be larger
+                idx = self.edges.index((n, n2))
+                arr[idx] = -1
 
-                self.a_ub.append(arr)
-                self.b_ub.append(0)
+            self.a_ub.append(arr)
+            self.b_ub.append(0)
 
         # For Non-Terminals for any selected outgoing edge, there must be an incoming edge
         for n in self.nodes:
-            if n not in self.terminals and n != self.root:
-                for na in nx.neighbors(steiner.graph, n):
-                    arr = [0] * len(self.edges)
-                    idx_out = self.edges.index((n, na))
-                    arr[idx_out] = 1
-                    for n2 in nx.neighbors(steiner.graph, n):
-                        # Treat as incoming edge
-                        idx_in = self.edges.index((n2, n))
-                        arr[idx_in] = -1
+            for na in nx.neighbors(steiner.graph, n):
+                arr = [0] * len(self.edges)
+                idx_out = self.edges.index((n, na))
+                arr[idx_out] = 1
+                for n2 in nx.neighbors(steiner.graph, n):
+                    # Treat as incoming edge
+                    idx_in = self.edges.index((n2, n))
+                    arr[idx_in] = -1
 
-                    self.a_ub.append(arr)
-                    self.b_ub.append(0)
+                self.a_ub.append(arr)
+                self.b_ub.append(0)
 
         # Not both arcs of an edge can be in a solution (the result is a directed tree)
         for i in range(0, len(self.edges), 2):
@@ -109,19 +107,29 @@ class SolverBc:
             self.b_ub.append(1)
 
     def solve(self):
-        self.solve_branch(set(), [])
+        self.solve_branch(set(), set())
         print "Solution found {}".format(self.upper_bound)
         return self.upper_bound_graph, self.upper_bound
 
     def solve_branch(self, cuts, fixed):
-        c_a_ub = list(self.a_ub)
-        c_b_ub = list(self.b_ub)
         c_bounds = list(self.bounds)
+        c_a_eq = list(self.a_eq)
+        c_b_eq = list(self.b_eq)
 
-        for (idx, val) in fixed:
-            c_bounds[idx] = (val, val)
+        for (n, val) in fixed:
+            arr = [0] * len(self.edges)
+            for n2 in nx.neighbors(self.steiner.graph, n):
+                arr[self.edges.index((n2, n))] = 1
+
+            c_a_eq.append(arr)
+            c_b_eq.append(val)
 
         while True:
+            last_result = None
+
+            c_a_ub = list(self.a_ub)
+            c_b_ub = list(self.b_ub)
+
             for c_cut in cuts:
                 cut_list = [0] * len(self.edges)
 
@@ -132,17 +140,51 @@ class SolverBc:
                 c_a_ub.append(cut_list)
 
             # First solve relaxation
-            lp = sp.linprog(self.costs, c_a_ub, c_b_ub, self.a_eq, self.b_eq, c_bounds, options={'maxiter': 1000})
+            lp = sp.linprog(self.costs, c_a_ub, c_b_ub, c_a_eq, c_b_eq, c_bounds, options={'maxiter': 10000})
 
-            if lp.success and self.check_solved(lp):
-                return
+            if lp.success:
+                last_result = lp.x
+
+                if self.check_solved(lp):
+                    return
 
             # Check for branching conditions
             if not lp.success or 0 == self.find_cuts(lp, cuts):
+                branch_at = None
+                if last_result is not None:
+                    c_min = sys.maxint, -1
+                    # Find node where incoming edges are closest to 0.5
+                    for n, nb in ((n, self.steiner.graph[n]) for n in self.nodes):
+                        val = sum([abs(last_result[self.edges.index((n2, n))] - 0.5) for n2 in nb])
+                        if val < c_min[0]:
+                            c_min = val, n
+                    branch_at = c_min[1]
+                else:
+                    c_max = 0, -1
+                    for n in self.nodes:
+                        if (n, 0) not in fixed and ((n, 1) not in fixed):
+                            if self.steiner.graph.degree[n] > c_max[0]:
+                                c_max = self.steiner.graph.degree[n], n
+                    branch_at = c_max[1]
+
                 print "Branching"
+                rules = set(fixed)
+                rules2 = set(fixed)
+                rules.add((branch_at, 0))
+                rules2.add((branch_at, 1))
+                c_a_ub = None
+                c_a_eq = None
+                c_bounds = None
+                c_b_eq = None
+                c_b_ub = None
+                self.solve_branch(set(cuts), rules)
+                self.solve_branch(set(cuts), rules2)
                 return
 
     def check_solved(self, result):
+        if result.fun >= self.upper_bound:
+            return True
+
         epsilon = 0.0000001
 
         # Check if every var is either 0 or 1
@@ -165,6 +207,7 @@ class SolverBc:
             if result[1] < self.upper_bound:
                 self.upper_bound = result[1]
                 self.upper_bound_graph = result[0]
+                print "New best result {}".format(result[1])
 
             return True
 
