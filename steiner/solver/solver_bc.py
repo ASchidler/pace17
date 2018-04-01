fb1f359136e0fd0d5ad2f3c5cd9e6619b7a5f7bc
Andre Schidler <andre@schidler.at>
2018-04-01 14:55:33 +0200

    Rudimentary B&C implementation that supports cutting

---
+++ steiner/solver/solver_bc.py
@@ -0,0 +1,193 @@
+import networkx as nx
+import scipy.optimize as sp
+
+
+class SolverBc:
+    def __init__(self, steiner, terminals, heuristics):
+        self.steiner = steiner
+        self.terminals = list(terminals)
+        self.heuristics = heuristics
+        self.upper_bound = self.steiner.get_approximation().cost
+        self.upper_bound_graph = self.steiner.get_approximation().tree
+        self.a_ub = []
+        self.b_ub = []
+        self.a_eq = []
+        self.b_eq = []
+
+        self.nodes = list(nx.nodes(self.steiner.graph))
+
+        self.terminals.sort()
+        self.nodes.sort()
+
+        self.root = self.terminals.pop()
+
+        # Create cost and edge array. Directional, i.e. each edge is converted to two arcs
+        self.costs = []
+        self.edges = []
+
+        for (u, v, d) in steiner.graph.edges(data='weight'):
+            self.costs.append(d)
+            self.costs.append(d)
+            self.edges.append((u, v))
+            self.edges.append((v, u))
+
+        self.bounds = [(0, 1)] * len(self.edges)
+
+        # Add default rules
+        # No incoming edges to root and at least on outgoing for the root node
+        root_arr = [0] * len(self.edges)
+        for n in nx.neighbors(steiner.graph, self.root):
+            # Treat as incoming edge
+            idx = self.edges.index((n, self.root))
+            self.bounds[idx] = (0, 0)
+            # Outgoing edge
+            idx = self.edges.index((self.root, n))
+            root_arr[idx] = -1
+
+        self.a_ub.append(root_arr)
+        self.b_ub.append(-1)
+
+        # Terminals have to have exactly one incoming edge
+        for t in self.terminals:
+            arr = [0] * len(self.edges)
+            for n in nx.neighbors(steiner.graph, t):
+                idx = self.edges.index((n, t))
+                arr[idx] = 1
+
+            self.a_eq.append(arr)
+            self.b_eq.append(1)
+
+        # Non-Terminals have at max one incoming edge
+        for n in self.nodes:
+            if n not in self.terminals and n != self.root:
+                arr = [0] * len(self.edges)
+                for n2 in nx.neighbors(steiner.graph, n):
+                    # Treat as incoming edge
+                    idx = self.edges.index((n2, n))
+                    arr[idx] = 1
+
+                self.a_ub.append(arr)
+                self.b_ub.append(1)
+
+        # For Non-Terminals, more or equal have to exit than entered (according to paper, this strengthens relaxations)
+        for n in self.nodes:
+            if n not in self.terminals and n != self.root:
+                arr = [0] * len(self.edges)
+                for n2 in nx.neighbors(steiner.graph, n):
+                    # Treat as incoming edge
+                    idx = self.edges.index((n2, n))
+                    arr[idx] = 1
+                    # Outgoing edge, should be larger
+                    idx = self.edges.index((n, n2))
+                    arr[idx] = -1
+
+                self.a_ub.append(arr)
+                self.b_ub.append(0)
+
+        # For Non-Terminals for any selected outgoing edge, there must be an incoming edge
+        for n in self.nodes:
+            if n not in self.terminals and n != self.root:
+                for na in nx.neighbors(steiner.graph, n):
+                    arr = [0] * len(self.edges)
+                    idx_out = self.edges.index((n, na))
+                    arr[idx_out] = 1
+                    for n2 in nx.neighbors(steiner.graph, n):
+                        # Treat as incoming edge
+                        idx_in = self.edges.index((n2, n))
+                        arr[idx_in] = -1
+
+                    self.a_ub.append(arr)
+                    self.b_ub.append(0)
+
+        # Not both arcs of an edge can be in a solution (the result is a directed tree)
+        for i in range(0, len(self.edges), 2):
+            arr = [0] * len(self.edges)
+            arr[i] = 1
+            arr[i+1] = 1
+
+            self.a_ub.append(arr)
+            self.b_ub.append(1)
+
+    def solve(self):
+        self.solve_branch(set(), [])
+        print "Solution found {}".format(self.upper_bound)
+        return self.upper_bound_graph, self.upper_bound
+
+    def solve_branch(self, cuts, fixed):
+        c_a_ub = list(self.a_ub)
+        c_b_ub = list(self.b_ub)
+
+        while True:
+            for c_cut in cuts:
+                cut_list = [0] * len(self.edges)
+
+                for c_idx in c_cut:
+                    cut_list[c_idx] = -1
+
+                c_b_ub.append(-1)
+                c_a_ub.append(cut_list)
+
+            # First solve relaxation
+            lp = sp.linprog(self.costs, c_a_ub, c_b_ub, self.a_eq, self.b_eq, self.bounds, options={'maxiter': 1000})
+
+            if not lp.success:
+                raise RuntimeError("Simplex Failed {}".format(lp.status))
+
+            # Check if solved
+            epsilon = 0.0000001
+
+            # Check if every var is either 0 or 1
+            violated_vars = 0
+            for c_x in lp.x:
+                if (c_x - epsilon) > 0 and (c_x + epsilon) < 1:
+                    violated_vars += 1
+
+            print "Simplex run, {} violated vars".format(violated_vars)
+
+            if violated_vars == 0:
+                result_graph = nx.Graph()
+                for i in range(0, len(lp.x)):
+                    if lp.x[i] + epsilon >= 1:
+                        u, v = self.edges[i][0], self.edges[i][1]
+                        result_graph.add_edge(u, v, weight=self.steiner.graph[u][v]['weight'])
+
+                result = result_graph, result_graph.size(weight='weight')
+
+                if result[1] < self.upper_bound:
+                    self.upper_bound = result[1]
+                    self.upper_bound_graph = result[0]
+
+                return
+
+            # Find cuts
+            cuts_prev = len(cuts)
+            for t in self.terminals:
+                cut_graph = nx.DiGraph()
+                for i in range(0, len(lp.x)):
+                    c_edge = self.edges[i]
+                    cut_graph.add_edge(c_edge[0], c_edge[1], capacity=lp.x[i] + 0.000001)
+
+                cut_val, cut_partition = nx.minimum_cut(cut_graph, self.root, t)
+                # Violated cut
+                while cut_val < 1:
+                    cut_set = set()
+                    # Find all incoming edges to the cut
+                    for n in cut_partition[1]:
+                        for n2 in nx.neighbors(self.steiner.graph, n):
+                            if n2 not in cut_partition[1]:
+                                idx = self.edges.index((n2, n))
+                                cut_set.add(idx)
+                                cut_graph[n2][n]['capacity'] = 1.000001
+
+                    # These steps are necessary to guarantee no duplicate cuts exist
+                    c_cut = list(cut_set)
+                    c_cut.sort()
+                    cuts.add(tuple(c_cut))
+
+                    cut_val, cut_partition = nx.minimum_cut(cut_graph, self.root, t)
+
+            if len(cuts) - cuts_prev == 0:
+                print "No solution found"
+                return
+
+            print "{} cuts found".format(len(cuts) - cuts_prev)
---
+++ steiner/solver_test.py
@@ -0,0 +1,37 @@
+import time
+
+import networkx as nx
+import iparser as pp
+
+import config as cfg
+import reduction.terminals as terminals
+import solver.label_store as ls
+import steiner_graph as sg
+import component_finder as cf
+import reduction.degree as dg
+import sys
+import solver.solver_bc as bc
+
+""" This runner runs all the public instances with debug oparser """
+
+
+def process_file(filename):
+    """ Processes a file. Parses, reduces and solves it. Includes verbose oparser"""
+
+    f = open(filename, "r")
+    steinerx = pp.parse_pace_file(f)
+    solver = bc.SolverBc(steinerx, steinerx.terminals, None)
+    solution = solver.solve()
+
+
+# Exceptionally slow instances: 101, 123, 125 (125 is currently the maximum)
+for i in range(1, 2):
+    file_path = "..\instances\lowTerm\instance{0:03d}.gr"
+    if i % 2 == 1:
+        sys.setcheckinterval(1000)
+        current_file = file_path.format(i)
+        print current_file
+        start = time.time()
+        e1 = process_file(current_file)
+        print "Done in " + str(time.time() - start)
+        print ""
