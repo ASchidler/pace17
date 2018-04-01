d4af537ac546a959656bc3759e2df6f6213faee6
Andre Schidler <andre@schidler.at>
2018-04-01 18:35:45 +0200

    Refined distance reductions

--- steiner/reduction/terminal_distance.py
+++ steiner/reduction/terminal_distance.py
@@ -1,27 +1,27 @@
 import networkx as nx
 
+
 class CostVsTerminalDistanceReduction:
     """ Removes all edges that are longer than the maximum distance between two terminals """
+    def __init__(self):
+        self.val = None
 
     def reduce(self, steiner):
         # Edge case, only one terminal
         if len(steiner.terminals) == 1:
             return 0
 
+        # Find max distance
+        if self.val is None:
+            g = nx.Graph()
+            [g.add_edge(t1, t2, weight=steiner.get_lengths(t1, t2))
+                for t1 in steiner.terminals for t2 in steiner.terminals if t2 > t1]
+
+            self.val = max([d for (u, v, d) in nx.maximum_spanning_edges(g)])
+
         terminal_edges = 0
-        max_val = 0
-        ts = list(steiner.terminals)
-        for i in range(0, len(ts)):
-            t1 = ts[i]
-
-            for j in range(i+1, len(ts)):
-                t2 = ts[j]
-
-                dist = steiner.get_lengths(t1, t2)
-                max_val = max(max_val, dist)
-
         for (u, v, d) in list(steiner.graph.edges(data='weight')):
-            if d > max_val:
+            if d > self.val:
                 steiner.remove_edge(u, v)
                 terminal_edges = terminal_edges + 1
 
