5df018c9126c81ba32fb2251149e4764dafdfec2
Andre Schidler <andre@schidler.at>
2018-04-04 21:10:18 +0200

    First draft of key vertex deletion

--- steiner/steiner_approximation.py
+++ steiner/steiner_approximation.py
@@ -2,12 +2,13 @@ import sys
 import networkx as nx
 import heapq
 from collections import defaultdict
+from itertools import chain
 
 
 class VoronoiPartition:
     def __init__(self, source, target):
         self.regions = {}
-        self.closest = {}
+        self._closest = {}
         self.source = source
         self._tmp_regions = defaultdict(lambda: {})
         self._tmp_closest = {}
@@ -28,7 +29,7 @@ class VoronoiPartition:
             if el[1] not in visited:
                 visited.add(el[1])
                 self.regions[el[2]][el[1]] = (el[0], el[3])
-                self.closest[el[1]] = el[2]
+                self._closest[el[1]] = el[2]
 
                 for n in nx.neighbors(source, el[1]):
                     if n not in visited:
@@ -57,7 +58,7 @@ class VoronoiPartition:
         # Initialize dijkstra s.t. the boundary nodes are added with the distance to the adjacent center
         for rep in repair_nodes:
             for nb in nx.neighbors(self.source, rep):
-                c_closest = self.closest[nb]
+                c_closest = self._closest[nb]
                 if c_closest not in intermediaries:
                     cost = self.regions[c_closest][nb][0] + self.source[rep][nb]['weight']
                     heapq.heappush(queue, [cost, rep, c_closest, nb])
@@ -76,7 +77,7 @@ class VoronoiPartition:
                         heapq.heappush(queue, [cost, nb, el[2], el[1]])
 
     def extract_path(self, n):
-        t = self._tmp_closest[n] if n in self._tmp_closest else self.closest[n]
+        t = self._tmp_closest[n] if n in self._tmp_closest else self._closest[n]
         vor = self._tmp_regions[t] if t in self._tmp_regions else self.regions[t]
 
         path = []
@@ -97,7 +98,13 @@ class VoronoiPartition:
         if n in self._tmp_closest:
             return self._tmp_regions[self._tmp_closest[n]][n][0]
         else:
-            return self.regions[self.closest[n]][n][0]
+            return self.regions[self._closest[n]][n][0]
+
+    def closest(self, n):
+        if n in self._tmp_closest:
+            return self._tmp_closest[n]
+
+        return self._closest[n]
 
 
 class SteinerApproximation:
@@ -106,21 +113,27 @@ class SteinerApproximation:
         self.cost = sys.maxint
         self.tree = None
 
-        # TODO: Find a more sophisticated selection strategy?
         limit = min(20, len(steiner.terminals))
+        ts = list(steiner.terminals)
         for i in xrange(0, limit):
-            result = self.calculate(steiner, max(len(steiner.terminals) / limit, 1) * i)
+            start_node = ts[max(len(steiner.terminals) / limit, 1) * i]
+            result = self.calculate(steiner, start_node)
             if result[1] < self.cost:
                 self.cost = result[1]
                 self.tree = result[0]
 
         print "Original {}".format(self.cost)
-        self.path_exchange(steiner, False)
-        print "PE {}".format(self.cost)
-        self.vertex_insertion(steiner)
-        print "VI {}".format(self.cost)
+        prev = 0
+        while prev != self.cost:
+            prev = self.cost
+            self.keyvertex_deletion(steiner)
+            print "VD {}".format(self.cost)
+            self.path_exchange(steiner, False)
+            print "PE {}".format(self.cost)
+            self.vertex_insertion(steiner)
+            print "VI {}".format(self.cost)
 
-    def calculate(self, steiner, start):
+    def calculate(self, steiner, start_node):
         # Solution init
         tree = nx.Graph()
         cost = 0
@@ -130,7 +143,10 @@ class SteinerApproximation:
         nodes = set()
 
         # Set start node
-        nodes.add(queue.pop(start))
+        if start_node in steiner.terminals:
+            queue.remove(start_node)
+
+        nodes.add(start_node)
 
         while len(queue) > 0:
             # Find terminal with minimal distance to tree
@@ -236,8 +252,8 @@ class SteinerApproximation:
 
         # Find bridging edges
         for (u, v, d) in steiner.graph.edges(data='weight'):
-            t1 = vor.closest[u]
-            t2 = vor.closest[v]
+            t1 = vor._closest[u]
+            t2 = vor._closest[v]
 
             if t1 != t2:
                 cost = d + vor.regions[t1][u][0] + vor.regions[t2][v][0]
@@ -247,7 +263,7 @@ class SteinerApproximation:
         for k, e in bridges.items():
             e.sort(key=lambda x: x[0])
 
-        # Keynodes
+        # Critical nodes
         key_nodes = set((x for x in nx.nodes(self.tree) if x in steiner.terminals or nx.degree(self.tree, x) > 2))
 
         # DFS into the tree
@@ -378,14 +394,217 @@ class SteinerApproximation:
         self.tree = g_prime
         self.cost = sum([d for (u, v, d) in g_prime.edges(data='weight')])
 
-    def descend(self, g, n, r):
-        lst = set()
-        lst.add(n)
+    def keyvertex_deletion(self, steiner):
+        bridges = {}
+        horizontal = {}
 
-        prev = 0
-        while prev != len(lst):
-            prev = len(lst)
-            n = [x for v in lst for x in nx.neighbors(g, v) if x != r]
-            [lst.add(x) for x in n]
+        # Pick root and create directed tree
+        root = next(iter(steiner.terminals))
+        d_tree = nx.dfs_tree(self.tree, root)
 
-        return lst
+        forbidden = set()
+        pinned = set()
+
+        # Find voronoi regions
+        for n in nx.nodes(self.tree):
+            bridges[n] = []
+            horizontal[n] = []
+
+        vor = VoronoiPartition(steiner.graph, self.tree)
+
+        # Find bridging edges and horizontal edges
+        for (u, v, d) in steiner.graph.edges(data='weight'):
+            t1 = vor.closest(u)
+            t2 = vor.closest(v)
+
+            if t1 != t2:
+                cost = d + vor.regions[t1][u][0] + vor.regions[t2][v][0]
+                bridges[t1].append([cost, (u, v, d)])
+                bridges[t2].append([cost, (u, v, d)])
+
+                # Find lowest common ancestor
+                anc = nx.ancestors(d_tree, t1)
+                anc.add(t1)
+                c_n = t2
+                while c_n not in anc:
+                    c_n = d_tree.predecessors(c_n).next()
+
+                # If one the nodes is itself the nca, the edge is not horizontal
+                if c_n != t1 and c_n != t2:
+                    horizontal[c_n].append((u, v, d))
+
+        for k, e in bridges.items():
+            e.sort(key=lambda x: x[0])
+
+        critical_nodes = set((x for x in nx.nodes(self.tree) if x in steiner.terminals or nx.degree(self.tree, x) > 2))
+
+        def kv_rec(node, last):
+            new_last = node if node in critical_nodes else last
+
+            # DFS
+            child_solutions = []
+            for n2 in nx.neighbors(d_tree, node):
+                child_solutions.append(kv_rec(n2, new_last))
+
+            # Non-Key?
+            if len(child_solutions) <= 1:
+                subset = set() if len(child_solutions) == 0 else child_solutions[0][0]
+                intermediaries = [] if len(child_solutions) == 0 else child_solutions[0][1]
+
+                # Move border nodes up
+                if node != root:
+                    parent = d_tree.predecessors(node).next()
+                    bridges[parent] = list(heapq.merge(bridges[parent], bridges[node]))
+
+                # If terminal, node is critical. Therefore update subset and clear intermediary list
+                if node in steiner.terminals:
+                    subset.add(node)
+                    [subset.add(x) for x in intermediaries]
+                    return subset, [], node
+                # Otherwise add self to intermediary list
+                else:
+                    intermediaries.append(node)
+                    return subset, intermediaries, child_solutions[0][2]
+            # Key?
+            else:
+                parent_intermediaries = set()
+                c_node = d_tree.predecessors(node).next()
+                parent_path = [(node, c_node)]
+                while c_node != last:
+                    parent_intermediaries.add(c_node)
+                    nxt = d_tree.predecessors(c_node).next()
+                    parent_path.append((nxt, c_node))
+                    c_node = nxt
+
+                    # Create list of intermediaries. Treat current node as such and "repair" voronoi
+                child_intermediaries = set(x for l in child_solutions for x in l[1])
+                child_subsets = dict((tn, ss) for ss, se, tn in child_solutions)
+                intermediaries = parent_intermediaries.union(child_intermediaries)
+                intermediaries.add(node)
+
+                subsets = set(chain.from_iterable((ss for kv, ss in child_subsets.items())))
+                [subsets.add(x) for x in intermediaries]
+                subsets.add(node)
+
+                # Do not remove pinned
+                if any(x in pinned for x in intermediaries):
+                    return subsets, [], node
+
+                vor.repair(intermediaries)
+
+                # Use valid (i.e. not ending in an intermediary area) horizontal edges
+                candidate_edges = []
+                for (x, y, dist) in horizontal[node]:
+                    violated = any(x in vor.regions[l] for l in child_intermediaries)
+                    violated = violated or any(y in vor.regions[l] for l in child_intermediaries)
+
+                    if not violated:
+                        candidate_edges.append((x, y, dist))
+
+                # Clean boundary edges
+                for c_child, c_ss in child_subsets.items():
+                    while len(bridges[c_child]) > 0:
+                        total, (x, y, dist) = bridges[c_child][0]
+                        # Select those edges where one endpoint is below node and one is above (i.e. not between)
+                        x_sub = any(x in vor.regions[t] for t in c_ss)
+                        y_sub = any(y in vor.regions[t] for t in c_ss)
+                        x_int = any(x in vor.regions[t] for t in intermediaries)
+                        y_int = any(y in vor.regions[t] for t in intermediaries)
+
+                        # not (y_int or y_sub) means y is in top partition
+                        if not ((x_sub and not (y_int or y_sub)) or (y_sub and not (x_int or x_sub))):
+                            bridges[c_child].pop(0)
+                        else:
+                            candidate_edges.append((x, y, dist))
+                            break
+
+                # Ok now we have all potential edges, calculate the MST
+                shortest_edges = defaultdict(lambda: (sys.maxint, None))
+
+                # Find shortest paths
+                for (x, y, dist) in candidate_edges:
+                    # Find voronoi center
+                    tp1 = vor.closest(x)
+                    tp2 = vor.closest(y)
+
+                    # Find component
+                    c1 = [cx for (cx, ss) in child_subsets.items() if tp1 in ss]
+                    c1 = c1[0] if len(c1) > 0 else last
+                    c2 = [cx for (cx, ss) in child_subsets.items() if tp2 in ss]
+                    c2 = c2[0] if len(c2) > 0 else last
+
+                    if c1 > c2:
+                        c1, c2 = c2, c1
+
+                    total_cost = vor.dist(x) + vor.dist(y) + dist
+                    # It may happen that c1 == c2 since the edges in bridges[last] have not been cleaned up
+                    if c1 != c2 and shortest_edges[(c1, c2)][0] > total_cost:
+                        p1 = vor.extract_path(x)
+                        p2 = vor.extract_path(y)
+
+                        if p1[1] not in forbidden and p2[1] not in forbidden:
+                            shortest_edges[(c1, c2)] = (total_cost, (x, y, dist), p1, p2)
+
+                # Create graph to calculate mst
+                super_graph = nx.Graph()
+                [super_graph.add_edge(tp1, tp2, weight=total) for ((tp1, tp2), (total, edge, p1, p2)) in shortest_edges.items()]
+
+                # Check if any node is not possibly connected
+                if all(super_graph.has_node(x) for x in child_subsets.keys()) and super_graph.has_node(last):
+                    mst_edges = list(nx.minimum_spanning_edges(super_graph))
+                    mst_cost = sum((dist['weight'] for (x, y, dist) in mst_edges))
+
+                    # Calculate original cost
+                    child_edges = list(parent_path)
+                    for (ss, pth, src) in child_solutions:
+                        c_node = src
+                        for idx in range(0, len(pth)):
+                            nxt = pth[idx]
+                            child_edges.append((c_node, nxt))
+                            c_node = nxt
+
+                        child_edges.append((c_node, node))
+
+                    original_cost = sum(steiner.graph[x][y]['weight'] for (x, y) in child_edges)
+
+                    # Replace
+                    if original_cost > mst_cost:
+                        [self.tree.remove_edge(x, y) for (x, y) in child_edges]
+
+                        for (x, y, dist) in mst_edges:
+                            if x > y:
+                                x, y = y, x
+
+                            total, (xp, yp, dp), p1, p2 = shortest_edges[(x, y)]
+                            self.tree.add_edge(xp, yp, weight=dp)
+                            [self.tree.add_edge(cu, cv, weight=cd) for cu, cv, cd in p1[0]]
+                            [self.tree.add_edge(cu, cv, weight=cd) for cu, cv, cd in p2[0]]
+
+                            # Mark path endpoints
+                            pinned.add(p1[1])
+                            pinned.add(p2[1])
+
+                        # Remove orphaned
+                        [self.tree.remove_node(x) for x in intermediaries if self.tree.degree[x] == 0]
+
+                        if not nx.is_connected(self.tree) or not nx.is_tree(self.tree) or len(
+                                [x for x in steiner.terminals if not self.tree.has_node(x)]) > 0:
+                            print "Error"
+
+                        [forbidden.add(x) for x in subsets]
+                        [forbidden.add(x) for x in intermediaries]
+
+                vor.reset()
+
+                # Update node boundary. This is a cleanup task for after the iteration, but since we may return
+
+                for kv in child_subsets.keys():
+                    bridges[node] = heapq.merge(bridges[node], bridges[kv])
+                for kv in child_intermediaries:
+                    bridges[node] = heapq.merge(bridges[node], bridges[kv])
+
+                bridges[node] = list(bridges[node])
+
+                return subsets, [], node
+
+        kv_rec(root, root)
