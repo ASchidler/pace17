0402057d61923af8abc629ff9319e1a835ef6466
WRK.AT\a.schidler <a.schidler@wrk.at>
2018-04-05 17:09:21 +0200

    Small fix for component reduction

--- steiner/reduction/component.py
+++ steiner/reduction/component.py
@@ -6,7 +6,7 @@ class ComponentReduction:
 
     def reduce(self, steiner):
         track = len(nx.nodes(steiner.graph))
-        if not nx.is_connected(steiner.graph):
+        if len(nx.nodes(steiner.graph)) > 1 and not nx.is_connected(steiner.graph):
             for c in list(nx.connected_components(steiner.graph)):
                 found = False
                 for n in c:
