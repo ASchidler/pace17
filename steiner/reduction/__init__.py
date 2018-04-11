276012294b821a0a2e0d340da828b71ec35b77f5
Andre Schidler <andre@schidler.at>
2018-04-11 19:43:45 +0200

    New version of dual ascent

--- steiner/reduction/__init__.py
+++ steiner/reduction/__init__.py
@@ -1,3 +1,3 @@
 __all__ = ["component", "cut_reachability", "degree", "long_edges", "ntdk",
            "reachability", "terminal_distance", "voronoi", "terminals", "cut_reachability_edge", "voronoi_nodes", "sdc",
-           "bound_reductions"]
+           "bound_reductions", "dual_ascent"]
--- steiner/reduction/dual_ascent.py
+++ steiner/reduction/dual_ascent.py
@@ -1,5 +1,7 @@
 import networkx as nx
 import sys
+import heapq as hq
+
 
 class DualAscent:
     def reduce(self, steiner):
@@ -12,76 +14,94 @@ class DualAscent:
             if result[0] > max_r[2]:
                 max_r = (result[1], root, result[0])
 
-        terminal_dist = {}
-        for t in ts:
-            terminal_dist[t] = nx.single_source_dijkstra_path_length(max_r[0], t)
-
-        root_dist = terminal_dist.pop(max_r[1])
+        root_dist = nx.single_source_dijkstra_path_length(max_r[0], max_r[1])
+        vor = self.voronoi(max_r[0], [t for t in ts if t != max_r[2]])
 
         track = 0
-        for n in nx.nodes(steiner.graph):
-            if n not in steiner.terminals:
-                bnd = root_dist[n] + min((v[n] for (t, v) in terminal_dist.items()))
+        edges = set()
+        for (t, v) in vor.items():
+            for (n, d) in v.items():
+                # Theoretically the paths should be arc-disjoint -> possible tighter bound
+                bnd = root_dist[n] + d
 
                 if bnd + max_r[2] > steiner.get_approximation().cost:
+                    steiner.remove_node(n)
                     track += 1
+                else:
+                    for n2 in list(nx.neighbors(steiner.graph, n)):
+                        if max_r[2] + root_dist[n2] + d + max_r[0][n2][n]['weight'] > steiner.get_approximation().cost:
+                            if (n, n2) in edges:
+                                track += 1
+                                steiner.remove_edge(n, n2)
+                            else:
+                                edges.add((n2, n))
 
         return track
 
+    def voronoi(self, dg, ts):
+        voronoi = {t: {} for t in ts}
+
+        queue = [[0, t, t] for t in ts]
+        visited = set()
+
+        while len(visited) != len(nx.nodes(dg)):
+            el = hq.heappop(queue)
+
+            if el[1] in visited:
+                continue
+
+            visited.add(el[1])
+
+            voronoi[el[2]][el[1]] = el[0]
+
+            for n in dg.predecessors(el[1]):
+                if n not in visited:
+                    d = dg[n][el[1]]['weight']
+                    hq.heappush(queue, [el[0]+d, n, el[2]])
+
+        return voronoi
 
     def calc(self, steiner, root):
-        da_graph = steiner.graph.copy()
+        dg = steiner.graph.to_directed()
         main_cut = set()
         main_cut.add(root)
-        cuts = []
         bound = 0
 
-        for t in steiner.terminals:
-            if t != root:
-                c = set()
-                c.add(t)
-                cuts.append(c)
+        t_cuts = []
+        for t in (t for t in steiner.terminals if t != root):
+            c = set()
+            c.add(t)
+            t_cuts.append([1, c])
 
-        while len(cuts) > 0:
-            c_idx = -1
-            c_cut = None
-            c_min = sys.maxint
-            remove = False
-
-            for i in range(0, len(cuts)):
-                if len(cuts[i]) < c_min:
-                    c_cut = cuts[i]
-                    c_idx = i
-                    c_min = len(c_cut)
-
+        while len(t_cuts) > 0:
+            c_cut = hq.heappop(t_cuts)[1]
             new_cut = set(c_cut)
-            cuts.pop(c_idx)
 
             delta = sys.maxint
             for n in c_cut:
-                for n2 in nx.neighbors(da_graph, n):
+                for n2 in dg.predecessors(n):
                     if n2 not in c_cut:
-                        delta = min(delta, da_graph[n][n2]['weight'])
-
+                        delta = min(delta, dg[n2][n]['weight'])
             bound += delta
 
+            remove = False
             for n in c_cut:
-                for n2 in nx.neighbors(da_graph, n):
+                for n2 in dg.predecessors(n):
                     if n2 not in c_cut:
-                        d = da_graph[n][n2]['weight']
+                        d = dg[n2][n]['weight']
                         d = max(0, d - delta)
-                        da_graph[n][n2]['weight'] = d
+                        dg[n2][n]['weight'] = d
 
-                        # Add node to cut
-                        if d == 0 and n2 not in new_cut:
+                        if d <= 0:
                             if n2 in main_cut:
                                 remove = True
                             else:
                                 found = False
-                                # Is already in cut?
-                                for j in range(0, len(cuts)):
-                                    if n2 in cuts[j]:
-                                        new_cut = new_cut.union(cuts.pop(j))
+
+                                for i in range(0, len(t_cuts)):
+                                    if n2 in t_cuts[i][1]:
+                                        new_cut = new_cut.union(t_cuts.pop(i)[1])
+                                        hq.heapify(t_cuts)
                                         found = True
                                         break
 
@@ -91,7 +111,10 @@ class DualAscent:
             if remove:
                 main_cut = main_cut.union(new_cut)
             else:
-                cuts.append(new_cut)
+                hq.heappush(t_cuts, [len(new_cut), new_cut])
 
-        return bound, da_graph
+        return bound, dg
+
+    def post_process(self, solution):
+        return solution, False
 
