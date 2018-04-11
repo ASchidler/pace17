7734482566f846d7222083b9e70659f3ec77d91a
Andre Schidler <andre@schidler.at>
2018-04-12 00:02:00 +0200

    Dual Ascent providing a better lower bound

--- steiner/reduction/dual_ascent.py
+++ steiner/reduction/dual_ascent.py
@@ -7,6 +7,8 @@ class DualAscent:
     def reduce(self, steiner):
         ts = list(steiner.terminals)
         max_r = (None, None, 0)
+        track = 0
+
         for i in range(0, min(10, len(ts))):
             root = ts[i]
 
@@ -17,19 +19,17 @@ class DualAscent:
         root_dist = nx.single_source_dijkstra_path_length(max_r[0], max_r[1])
         vor = self.voronoi(max_r[0], [t for t in ts if t != max_r[2]])
 
-        track = 0
         edges = set()
+        limit = steiner.get_approximation().cost - max_r[2]
         for (t, v) in vor.items():
             for (n, d) in v.items():
                 # Theoretically the paths should be arc-disjoint -> possible tighter bound
-                bnd = root_dist[n] + d
-
-                if bnd + max_r[2] > steiner.get_approximation().cost:
+                if root_dist[n] + d > limit:
                     steiner.remove_node(n)
                     track += 1
                 else:
                     for n2 in list(nx.neighbors(steiner.graph, n)):
-                        if max_r[2] + root_dist[n2] + d + max_r[0][n2][n]['weight'] > steiner.get_approximation().cost:
+                        if root_dist[n2] + max_r[0][n2][n]['weight'] + d > limit:
                             if (n, n2) in edges:
                                 track += 1
                                 steiner.remove_edge(n, n2)
@@ -66,15 +66,15 @@ class DualAscent:
         main_cut = set()
         main_cut.add(root)
         bound = 0
-
         t_cuts = []
+
         for t in (t for t in steiner.terminals if t != root):
             c = set()
             c.add(t)
             t_cuts.append([1, c])
 
         while len(t_cuts) > 0:
-            c_cut = hq.heappop(t_cuts)[1]
+            old_size, c_cut = hq.heappop(t_cuts)
             new_cut = set(c_cut)
 
             delta = sys.maxint
@@ -84,34 +84,41 @@ class DualAscent:
                         delta = min(delta, dg[n2][n]['weight'])
             bound += delta
 
-            remove = False
+            hq.heappush(t_cuts, [len(new_cut), new_cut])
+
             for n in c_cut:
+                any_in = False
                 for n2 in dg.predecessors(n):
+                    any_in = True
                     if n2 not in c_cut:
                         d = dg[n2][n]['weight']
                         d = max(0, d - delta)
                         dg[n2][n]['weight'] = d
 
                         if d <= 0:
-                            if n2 in main_cut:
-                                remove = True
-                            else:
-                                found = False
+                            all_containing = []
+                            for i in reversed(range(0, len(t_cuts))):
+                                if n in t_cuts[i][1]:
+                                    all_containing.append(t_cuts.pop(i)[1])
 
-                                for i in range(0, len(t_cuts)):
-                                    if n2 in t_cuts[i][1]:
-                                        new_cut = new_cut.union(t_cuts.pop(i)[1])
-                                        hq.heapify(t_cuts)
-                                        found = True
+                            for c_con in all_containing:
+                                queue = [n2]
+                                remove = False
+
+                                while len(queue) > 0:
+                                    c_elem = queue.pop()
+                                    if c_elem in steiner.terminals:
+                                        remove = True
                                         break
 
-                                if not found:
-                                    new_cut.add(n2)
+                                    c_con.add(c_elem)
+                                    [queue.append(x) for x in dg.predecessors(c_elem) if x not in c_con and
+                                                   dg[x][c_elem]['weight'] == 0]
 
-            if remove:
-                main_cut = main_cut.union(new_cut)
-            else:
-                hq.heappush(t_cuts, [len(new_cut), new_cut])
+                                if not remove:
+                                    t_cuts.append([len(c_con), c_con])
+
+            hq.heapify(t_cuts)
 
         return bound, dg
 
