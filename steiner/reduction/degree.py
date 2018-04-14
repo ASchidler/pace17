29a39d8d77d65c71116533828deda12b4f1fe7e6
Andre Schidler <andre@schidler.at>
2018-04-14 15:16:31 +0200

    Merged terminal and degree reduction

--- steiner/reduction/degree.py
+++ steiner/reduction/degree.py
@@ -6,6 +6,9 @@ class DegreeReduction:
 
     def __init__(self):
         self._removed = {}
+        self._contracted = []
+        self._selected = []
+        self._done = False
 
     def reduce(self, steiner, cnt, last_run):
         track = len(steiner.graph.nodes)
@@ -14,11 +17,24 @@ class DegreeReduction:
 
         while old > len(steiner.graph.nodes):
             old = len(steiner.graph.nodes)
-            for n, degree in steiner.graph.degree():
-                if degree == 1 and n not in steiner.terminals:
-                    steiner.remove_node(n)
-                    break
-                elif degree == 2 and n not in steiner.terminals:
+            for n in list(steiner.graph.nodes):
+                if not steiner.graph.has_node(n):
+                    continue
+
+                dg = steiner.graph.degree(n)
+                if dg == 1:
+                    # Nodes of degree1 => remove
+                    if n not in steiner.terminals:
+                        steiner.remove_node(n)
+                    # Terminals of degree 1 => Simply contract
+                    else:
+                        nb = next(steiner.graph.neighbors(n))
+                        self._contracted.append((n, nb, steiner.graph[n][nb]['weight']))
+                        steiner.move_terminal(n, nb)
+                        steiner.remove_node(n)
+
+                # Nodes of degree 2 => merge
+                elif dg == 2 and n not in steiner.terminals:
                     nb = list(steiner.graph.neighbors(n))
                     w1 = steiner.graph[n][nb[0]]['weight']
                     w2 = steiner.graph[n][nb[1]]['weight']
@@ -26,12 +42,35 @@ class DegreeReduction:
                     if steiner.add_edge(nb[0], nb[1], w1+w2):
                         self._removed[(nb[0], nb[1], w1 + w2)] = [(nb[0], n, w1), (nb[1], n, w2)]
                     steiner.remove_node(n)
-                    break
+
+                # Terminals of higher degree? Merge if the nearest node is a terminal
+                elif dg >= 2 and n in steiner.terminals:
+                    min_val, min_nb = maxint, None
+                    for n2 in steiner.graph.neighbors(n):
+                        w = steiner.graph[n][n2]['weight']
+
+                        if w < min_val or (w == min_val and n2 in steiner.terminals):
+                            min_val, min_nb = w, n2
+
+                    if min_nb in steiner.terminals:
+                        self._contracted.append((n, min_nb, min_val))
+
+                        for e in steiner.contract_edge(n, min_nb):
+                            self._selected.append(e)
 
         return track - len(steiner.graph.nodes)
 
     def post_process(self, solution):
         change = False
+        cost = solution[1]
+
+        if not self._done:
+            for (n1, n2, w) in self._removed:
+                solution[0].add_edge(n1, n2, weight=w)
+                cost += w
+                change = True
+            self._done = True
+
         for (k, v) in self._removed.items():
             if solution[0].has_edge(k[0], k[1]) and solution[0][k[0]][k[1]]['weight'] == k[2]:
                 solution[0].remove_edge(k[0], k[1])
@@ -39,4 +78,11 @@ class DegreeReduction:
                 solution[0].add_edge(v[1][0], v[1][1], weight=v[1][2])
                 change = True
 
-        return solution, change
+        for (e1, e2) in self._selected:
+            if solution[0].has_edge(e1[0], e1[1]):
+                if solution[0][e1[0]][e1[1]]['weight'] == e1[2]:
+                    solution[0].remove_edge(e1[0], e1[1])
+                    solution[0].add_edge(e2[0], e2[1], weight=e2[2])
+                    change = True
+
+        return (solution[0], cost), change
