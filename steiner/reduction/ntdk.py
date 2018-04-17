cbd4948faa551ad4e0a86197e4483b72e854b086
Andre Schidler <andre@schidler.at>
2018-04-17 16:44:22 +0200

    Optimized NTDK

--- steiner/reduction/ntdk.py
+++ steiner/reduction/ntdk.py
@@ -20,47 +20,56 @@ class NtdkReduction:
             steiner.requires_steiner_dist(1)
 
         track = len(steiner.graph.nodes)
+        nbs = steiner.graph._adj
 
         for n in list(steiner.graph.nodes):
             if n not in steiner.terminals and 2 < steiner.graph.degree(n) <= 4:
-                nb = list(steiner.graph.neighbors(n))
-                true_for_all = True
-
-                nb.sort()
-                total_edge_sum = sum(steiner.graph[n][b]['weight'] for b in nb)
+                nb = nbs[n].items()
+                total_edge_sum = sum(dta['weight'] for (x, dta) in nb)
 
                 # Calc distances, more memory efficient than calculating it all beforehand
                 if not self._restricted:
-                    dist = {(x, y): self.modified_dijkstra(steiner, x, y, total_edge_sum, False) for x in nb for y in nb if y > x}
+                    dist = {(x, y): self.modified_dijkstra(steiner, x, y, total_edge_sum, False) for (x, tt1) in nb for (y, tt2) in nb if y > x}
                 else:
-                    dist = {(x, y): steiner.get_steiner_lengths(x, y, 0) for x in nb for y in nb if y > x}
+                    dist = {(x, y): steiner.get_steiner_lengths(x, y, 0) for (x, tt1) in nb for (y, tt2) in nb if y > x}
 
-                # Powersets
-                # TODO: This could be done faster, i.e. don't create powersets of cardinality < 3
-                for power_set in xrange(1, 1 << len(nb)):
-                    # Create complete graph
-                    power_graph = Graph()
-                    edge_sum = 0
+                # Choose the easier calculation for the case of degree 3
+                if len(nb) == 3:
+                    dist_itm = dist.values()
+                    nb_sum = min([dist_itm[0] + dist_itm[1], dist_itm[0] + dist_itm[2], dist_itm[1] + dist_itm[2]])
+                    true_for_all = nb_sum <= total_edge_sum
+                else:
+                    true_for_all = True
+                    # Sort for access to dist dict
+                    nb.sort(key=lambda x: x[0])
 
-                    # Sets of size at least 3
-                    if bin(power_set).count("1") >= 3:
+                    # Powersets
+                    # Set num will be the index of the neighbor not to include, plus -1 for the full set
+                    # Only works for degree 4!
+                    for set_num in xrange(-1, len(nb)):
+                        power_graph = Graph()
+                        edge_sum = 0
+
                         for i in xrange(0, len(nb)):
-                            if ((1 << i) & power_set) > 0:
-                                n1 = nb[i]
-                                edge_sum = edge_sum + steiner.graph[n][n1]['weight']
+                            if i != set_num:
+                                n1, dta = nb[i]
+                                edge_sum += dta['weight']
 
                                 for j in xrange(i + 1, len(nb)):
-                                    if ((1 << j) & power_set) > 0:
-                                        n2 = nb[j]
+                                    if j != set_num:
+                                        n2 = nb[j][0]
                                         w = dist[(n1, n2)]
                                         power_graph.add_edge(n1, n2, weight=w)
 
                         mst_sum = sum(d['weight'] for (u, v, d) in minimum_spanning_edges(power_graph))
                         true_for_all = true_for_all and mst_sum <= edge_sum
 
+                        if not true_for_all:
+                            break
+
                 if true_for_all:
                     # Introduce artificial edges
-                    for (n1, n2) in ((x, y) for x in nb for y in nb if y > x):
+                    for (n1, n2) in ((x, y) for (x, tt1) in nb for (y, tt2) in nb if y > x):
                         c1 = steiner.graph[n][n1]['weight']
                         c2 = steiner.graph[n][n2]['weight']
                         if c1 + c2 <= dist[(n1, n2)]:
