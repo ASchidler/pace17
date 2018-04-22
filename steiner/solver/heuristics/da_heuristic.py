16f07758b563be9fe81ca2eaef8780bc27e2d392
Andre Schidler <andre@schidler.at>
2018-04-23 01:25:25 +0200

    Fixed DA heuristic

--- steiner/solver/heuristics/da_heuristic.py
+++ steiner/solver/heuristics/da_heuristic.py
@@ -13,13 +13,6 @@ class DaHeuristic:
         if len(ts) == 1:
             for t in ts:
                 return self.steiner.get_lengths(t, n)
-        # MST -> Exact
-        if len(ts) == 2:
-            ts = list(ts)
-            l1 = self.steiner.get_lengths(ts[0], n)
-            l2 = self.steiner.get_lengths(ts[1], n)
-            l3 = self.steiner.get_lengths(ts[1], ts[0])
-            return min(l1 + l2, min(l1 + l3, l2 + l3))
 
         # Calc steiner tree including n rooted at r
         r = self.solver.root_node
